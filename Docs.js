1d45f6462920a0d9d427da8aa909921fbf5fc75c
Gaurav Yadav <gauravyadavpro63@gmail.com>
2026-08-13 00:58:25 +0530

    basics

---
+++ Docs.js
@@ -0,0 +1,50 @@
+//    Next.js
+
+
+// Next.js is a React framework for building full-stack web applications.You use React 
+//  Components to build user interfaces, and Next.js for additional features and optimizations.
+
+// LIBRARY vs FRAMEWORK
+
+// Library:
+// - A collection of reusable code/functions.
+// - Developer controls the application flow.
+// - Developer calls the library when needed.
+// - Example: React.
+
+// Framework:
+// - Provides a complete structure for building applications.
+// - Framework controls much of the application flow.
+// - Developer writes code according to the framework's conventions.
+// - Example: Next.js.
+
+// Easy rule:
+// Library → You call it.
+// Framework → It calls you.
+
+
+// NEXT.JS
+
+// Next.js is a React framework used to build modern
+// full-stack web applications.
+
+// Next.js is built on top of React.
+
+// It provides features such as:
+// - File-based routing
+// - Server Components
+// - Client Components
+// - Server-side rendering
+// - Data fetching
+// - Route Handlers
+// - Image optimization
+// - Metadata/SEO
+// - Loading and error handling
+
+// React → UI library
+// Next.js → React framework
+
+// Easy definition:
+// Next.js = React + additional features and
+// structure for building complete web applications.
+
